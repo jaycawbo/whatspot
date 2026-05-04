d980675b478ec4ccce77a67af613a08e95780353
Jake <jake.k.shipman@gmail.com>
2026-05-04 14:14:54 -0400

    Minor filter and search orchestrator cleanup
    - Small fixups to FilterDialog and GlobalStateContext
    - Reshuffle searchOrchestrator logic

--- src/context/GlobalStateContext.jsx
+++ src/context/GlobalStateContext.jsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 const GlobalStateContext = createContext();
 
 const initialFilters = {
-  openNow: true,
+  openNow: false,
   priceLevels: [],
   cuisines: [],
   radius: 5,
